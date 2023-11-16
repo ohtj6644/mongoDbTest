c7849e836089dfa33ba838ffaace4d9aafb95233
ohtj <ohtj@rmsoft.kr>
2023-11-16 17:14:51 +0900

    first

---
+++ mong3/src/main/java/com/example/mong3/Mong3Application.java
@@ -0,0 +1,13 @@
+package com.example.mong3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mong3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mong3Application.class, args);
+	}
+
+}
---
+++ mong3/src/test/java/com/example/mong3/Mong3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mong3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mong3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
